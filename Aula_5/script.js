58bcb5a5e265df86a14ff80e79c5e041113116fb
murilozornek <zornekmurilo.fei@gmail.com>
2025-03-18 17:04:05 -0300

    Aula_5

---
+++ Aula_5/script.js
@@ -0,0 +1,49 @@
+// window.alert("hello world!")
+console.log("Hello World!")
+
+//let nome = prompt("Qual o seu nome?");
+//console.log(nome);
+
+//console.log("while");
+//let i = 0;
+//while (i<10){
+ //   console.log(i);
+    //i++;
+    //i = i +1
+ //   i +=1; 
+//}
+//console.log("for");
+//for(let i = 0; i < 10; i++){
+//    console.log(i);
+//}
+
+
+//console.log("Exercicio 1");
+//for(let i=1; i <100; i+= 2){
+//  console.log(i)
+//}
+    
+//console.log("Exercicio 2")
+//for(let i = 5; i <= 500; i+=5){
+//    console.log(i)
+//}
+
+//console.log("exercicio 3")
+//let n = parseInt(prompt("Digite um número inteiro"));
+//if(n<0){
+//    console.log("Número inválido")
+//}
+//for(let i = n; i>= 0; i--){ 
+//    console.log(i)
+//}
+
+console.log("Exercicio 4");
+let num = parseInt(prompt("Digite um número"));
+if(num<0)
+    console.log("Número inválido")
+
+let mult = 1;
+for(let i = num, i > 0; i-- ){
+    mult *= i;
+    console.log(i)(num)
+}
