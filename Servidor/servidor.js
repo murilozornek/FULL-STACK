eb8bd8db13680fb99a2078bacae132f57f4c9779
murilozornek <zornekmurilo.fei@gmail.com>
2025-04-29 16:52:39 -0300

    Alterações da aula 10

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -20,6 +20,24 @@ server.listen(80);
 
 console.log("Servidor rodando...".rainbow) 
 
+// //Exemplo de Bancos de dados 
+var mongodb = require("mongodb");
+const { INSPECT_MAX_BYTES } = require("buffer");
+
+
+const MongoClient = mongodb.MongoClient;
+
+const uri = 'mongodb+srv://zornekmurilo:161002@murilo.yen2yak.mongodb.net/?retryWrites=true&w=majority&appName=Murilo'
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+
+
+// Exemplo de Banco de dados
+var dbo = client.db("Murilo");
+var usuarios = dbo.collection("usuarios");
+
+
+
 
 //métodos e action 
 
@@ -33,16 +51,16 @@ app.post("/inicio", function(requisição, resposta){
 })
 
 
-//exemplo com get 
-// app.get("/cadastrar", function (requisição,resposta){
-//     let nome = requisição.query.nome; 
-//     let login = requisição.query.login;
-//     let senha = requisição.query.senha;
-//     let nasc = requisição.query.Nascimento;
+// exemplo com get 
+app.get("/cadastrar", function (requisição,resposta){
+    let nome = requisição.query.nome; 
+    let login = requisição.query.login;
+    let senha = requisição.query.senha;
+    let nasc = requisição.query.Nascimento;
 
-//     console.log(nome,login,senha,nasc)
+    console.log(nome,login,senha,nasc)
 
-// })
+})
 
 app.post("/cadastrar", function (requisição,resposta){
     let nome = requisição.body.nome; 
@@ -52,8 +70,52 @@ app.post("/cadastrar", function (requisição,resposta){
 
     console.log(nome,login,senha,nasc)
 
-    resposta.render("resposta",{nome,login,senha,nasc});
+    var data = { db_nome: nome, db_login: login, db_senha: senha, db_nasc: nasc };
+
+    usuarios.insertOne(data,function(err){
+        console.log(err)
+        if (err){
+            resposta.render("resposta",{status:"erro",nome,login,senha,nasc});
+        }else{
+            resposta.render("resposta",{status:"sucesso",nome,login,senha,nasc});
+        }
+
+        
+
+    })
+
 })
+
+app.post('/logar', function(requisição,resposta){
+    let login = requisição.body.login;
+    let senha = requisição.body.senha;
+
+    console.log(login,senha)
+
+    var data = {db_login: login, db_senha: senha}
+
+    usuarios.find(data).toArray(function(err, item){
+        console.log(item)
+        if(item.length == 0){
+            resposta.render("resposta_login",{status:"usuario/senha não encontrado"});
+            
+            //não encontrou usuario
+
+        }else if(err){
+            resposta.render("resposta_login",{status:"erro ao logar"});
+             //erro ao logar
+
+        }else{
+            resposta.render("resposta_login",{status:"Usuario encontrado"});
+            //usuario encontrado
+        }
+    })    
+
+})
+
+
+   
+
 
 
 app.get("/for_ejs",function(requisição,resposta){
@@ -63,13 +125,13 @@ app.get("/for_ejs",function(requisição,resposta){
 
 
 
-usuarios  = []
+ var usuarios_cadastrados  = 
 
 app.post("/cadastra", function(requisição, resposta){
     let nome_usuario = requisição.body.nome_usuario;
     let senha = requisição.body.senha;
 
-    usuarios.push({
+    usuarios_cadastrados.push({
         nome_usuario: nome_usuario,
         senha: senha
     })
@@ -85,13 +147,18 @@ app.post("/login", function(requisição,resposta){
     let usuario = requisição.body.usuario;
     let senha = requisição.body.senha;
 
-    let usuarios_encontrados = usuarios.find( Uc=> Uc.nome_usuario === usuario && Uc.senha === senha)     // Uc é so um nome da variavel, poderia ser qualuqer outro nome .
-                                                                                                    //&& é igual o and no python 
-    if(usuarios_encontrados){
-        resposta.render("resultado_login", { nome: usuarios_encontrados.nome_usuario });
-    }                              
-    else{
-        resposta.send("Usuário ou senha inválidos ")
+    for( var i =0 ; i < usuarios_cadastrados.length; i++){
+        if(usuario == usuarios_cadastrados[i],[nome_usuario] && senha == usuarios_cadastrados[i][senha]){
+            usuarios_encontrados = true;
+            break;
+        }
     }
 
+    resposta.render("resultado_login",{usuario,senha,usuarios_cadastrados})
+
 })
+
+  
+                                                                                                   
+
+
